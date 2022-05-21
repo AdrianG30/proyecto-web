0a3702ed27971d01c88920841d2650f48c9d3fbf
Jhonatan Castillo <jhonatancastillox@gmail.com>
2022-05-20 20:25:09 -0500

    Se creo el modulo TokenUtils y se completo el google sign in para clientes

--- pages/api/v1/auth/sign-in/google.ts
+++ pages/api/v1/auth/sign-in/google.ts
@@ -1,13 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import getConnection from '../../../../../database/mongo';
-import { verifyGoogleToken } from '../../../../../utils/TokenUtils';
+import Cliente from '../../../../../models/Cliente';
+import Direccion from '../../../../../models/Direccion';
+import { verifyGoogleToken, createLoginTokens } from '../../../../../utils/TokenUtils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     await getConnection();
 
     if (req.method === 'POST') {
-        const ticket = await verifyGoogleToken(req.body.token);
+        const { idToken } = req.body;
+        const payload = await verifyGoogleToken(idToken);
 
-        res.json({ data: 'ok' });
+        if (payload) {
+            let cliente = await Cliente.findById(payload._id).populate({
+                path: 'direcciones',
+                model: Direccion,
+            });
+
+            if (!cliente) {
+                cliente = await Cliente.create(payload);
+                cliente.populate({ path: 'direcciones', model: Direccion });
+            }
+
+            const { accessToken, refreshToken } = createLoginTokens(cliente._id, cliente.email);
+
+            res.status(200).json({
+                accessToken,
+                refreshToken,
+                data: cliente,
+            });
+        }
+
+        res.status(401).end('Unauthorized');
     } else res.status(405).end(`Method ${req.method} not allowed.`);
 }
